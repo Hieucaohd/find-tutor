54ab41188ad5360cba51cb7631e398af58c61a5f
hieucaohd <hieucaohd@gmail.com>
2021-08-01 09:31:22 +0700

    hoan thanh file requiments.txt

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import RegisterView, VerifyEmail, Login
 
 from rest_framework_simplejwt.views import (
-    TokenRefreshSlidingView,
+    TokenRefreshView,
 )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('verify-email/', VerifyEmail.as_view(), name='verify-email'),
-    path('token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
