03b73b03d1959b895469052d5c53655ecfeadfac
hieucaohd <hieucaohd@gmail.com>
2021-08-14 15:35:25 +0700

    hoan thanh comment

--- comment/admin.py
+++ comment/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(CommentAboutTutorModel)
-admin.site.register(CommentAboutParentModel)
+admin.site.register(CommentAboutUserModel)
 admin.site.register(CommentAboutParentRoomModel)
