5805de73606bf89d6d9e62004e18eb755c06e6e2
hieucaohd <hieucaohd@gmail.com>
2021-10-18 09:23:51 +0700

    thu deploy tu dong

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-/home/ubuntu/findTutorProject/DjangoEnv/bin/pip install -r /home/ubuntu/findTutorProject/findTutor/requirements.txt
-
-# stop the supervisor running django app
+# start the supervisor running django app
 supervisorctl start findTutorDjango
--- scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# install all the dependencies in the requirements.txt
-/home/ubuntu/findTutorProject/DjangoEnv/bin/pip install -r /home/ubuntu/findTutorProject/findTutor/requirements.txt
