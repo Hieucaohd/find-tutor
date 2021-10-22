faa872f7a27d45669de39b90eb51fbbe7a85987c
hieucaohd <hieucaohd@gmail.com>
2021-10-22 18:21:54 +0700

    cho phep ten mien timgiaus.findtutorapp.website

--- findTeacherProject/settings.py
+++ findTeacherProject/settings.py
@@ -115,8 +115,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.29.102:3000",
     "http://192.168.29.103:3000",
     "https://timgiasu.vercel.app",
-    "https://findtutorapp.website",
-    "https://www.findtutorapp.website",
+    "https://timgiasu.findtutorapp.website",
+    "https://www.timgiasu.findtutorapp.website",
 ]
 
 ROOT_URLCONF = 'findTeacherProject.urls'
