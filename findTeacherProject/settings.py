10b664f9f53512c78bbff964a708e95eab6d9b85
hieucaohd <hieucaohd@gmail.com>
2021-10-22 16:06:23 +0700

    thu lan cuoi o nginx cho static file

--- findTeacherProject/settings.py
+++ findTeacherProject/settings.py
@@ -211,8 +211,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'api/staticrootfile')
-STATIC_URL = '/api/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticrootfile')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
