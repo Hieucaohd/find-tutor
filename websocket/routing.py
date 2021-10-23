a1fda13d860d6ef9ae3a1ad7ac020750b515cf84
hieucaohd <hieucaohd@gmail.com>
2021-10-23 16:01:01 +0700

    sua lai routing cua websocket

--- websocket/routing.py
+++ websocket/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from websocket.consumers import NotifyConsumer
 
 websocket_urlpatterns = [
-    re_path("ws/websocket/", NotifyConsumer.as_asgi()),
+    re_path("ws/notification/", NotifyConsumer.as_asgi()),
 ]
