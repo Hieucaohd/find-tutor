0210dbc22cf559d44780022a8fb4de2378339e8b
hieucaohd <hieucaohd@gmail.com>
2021-10-27 07:50:44 +0700

    them celery

--- findTeacherProject/settings.py
+++ findTeacherProject/settings.py
@@ -180,6 +180,10 @@ CHANNEL_LAYERS = {
 }
 
 
+# Celery config
+
+
+
 MONGO_URL = "mongodb+srv://hieucao192:" + urllib.parse.quote("Caotrunghieu@192") + "@authenticationtest.6lh8w.mongodb.net/userSearch?retryWrites=true&w=majority"
 
 
--- websocket/schema.py
+++ websocket/schema.py
@@ -16,17 +16,20 @@ class Query(graphene.ObjectType):
     all_room_notification = graphene.Field(ResolveAllRoomNotificationType, 
                                      page=graphene.Int(required=False),
                                      num_in_page=graphene.Int(required=False),
-                                     token=graphene.String(required=True),)
+                                     token=graphene.String(required=True),
+                                     is_new=graphene.Boolean(required=False),)
 
     def resolve_all_room_notification(root, info, **kwargs):
         page = kwargs.get('page', 1)
         num_in_page = kwargs.get('num_in_page', 10)
 
+        is_new = kwargs.get("is_new", True)
+
         collection = RoomNotificationModel().collection
 
         user_id_receive = info.context.user.id
         
-        cursor = collection.find({"user_id_receive": user_id_receive, "is_news": False }).sort('create_at', pymongo.DESCENDING)
+        cursor = collection.find({"user_id_receive": user_id_receive, "is_new": is_new }).sort('create_at', pymongo.DESCENDING)
 
         num_pages = int(cursor.count() / num_in_page) + 1
 
